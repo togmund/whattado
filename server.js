844cde17fbc15c4d9175eb70cd8c1260b8923901
Ted Ogmundson <togmund@gmail.com>
2019-11-17 22:21:27 +0000

    added userTodos.js route, and a button that retrireves all user_todo data from the user_todos table. Confirmed functions

--- server.js
+++ server.js
@@ -35,13 +35,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const userTodoRoutes = require("./routes/userTodos");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/userTodos", userTodoRoutes(db));
 // Note: mount other resources here, using the same pattern above
-
 
 // Home page
 // Warning: avoid creating more routes in this file!
