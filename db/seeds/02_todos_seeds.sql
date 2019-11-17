5f27e24be561acd3bdd4b51080fc9f79f20dc025
hildakh <h_khajehamiri@yahoo.com>
2019-11-17 22:40:20 +0000

    Seed files added to the folder

---
+++ db/seeds/02_todos_seeds.sql
@@ -0,0 +1,79 @@
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('Lucifer', 1, 540, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('Breaking Bad', 1, 390, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('Harry Potter', 1, 870, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('Lord of the Rings', 1, 287, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('Earls'' Kitchen', 2, 245, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('Cactus Club Cafe', 2, 435, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('Trees Organic Coffee', 2, 945, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('Thierry', 2, 946, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('bread', 3, 728, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('fruit', 3, 276, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('sofa', 3, 982, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('t-shirt', 3, 625, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('The Power', 4, 287, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('The Testaments', 4, 165, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('1984', 4, 324, 32);
+
+INSERT INTO
+  todos (name, type_id, api_id, api_sth)
+VALUES
+  ('World Without Fish', 4, 723, 32);
---
+++ db/seeds/05_users_seeds.sql
@@ -0,0 +1,47 @@
+INSERT INTO
+  users (name, email, password, avatar, group_id, time_id)
+VALUES
+  (
+    'Diandra	Logsdale',
+    'dlogsdale0@salon.com',
+    'password',
+    'face',
+    1,
+    2
+  );
+
+INSERT INTO
+  users (name, email, password, avatar, group_id, time_id)
+VALUES
+  (
+    'Flynn	Pierro',
+    'fpierro1@fotki.com',
+    'password',
+    'face',
+    2,
+    1
+  );
+
+INSERT INTO
+  users (name, email, password, avatar, group_id, time_id)
+VALUES
+  (
+    'Jemie	Laughtisse',
+    'jlaughtisse2@geocities.com',
+    'password',
+    'face',
+    3,
+    3
+  );
+
+INSERT INTO
+  users (name, email, password, avatar, group_id, time_id)
+VALUES
+  (
+    'Ciel	Linney',
+    'clinney3@google.ca',
+    'password',
+    'face',
+    4,
+    4
+  );
