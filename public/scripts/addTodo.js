146aa6a0f711d77c0fb0d2d6080f6ba279476dbc
Praneeth <praneethabc123@gmail.com>
2019-11-18 02:33:42 +0000

    Modified data that needs to be deleted

---
+++ public/scripts/addTodo.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // $('.fixed-action-btn').floatingActionButton();
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
+});
