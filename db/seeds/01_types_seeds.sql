b3c1633f613cec24fee1a95726f94f11209c6fdf
hildakh <h_khajehamiri@yahoo.com>
2019-11-18 00:17:40 +0000

    changed the colors of the types

--- db/seeds/01_types_seeds.sql
+++ db/seeds/01_types_seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO types (name, img, color, api_endpoint) VALUES ('movies & shows', 'movie_filter', '#AD1457', 'something');
+INSERT INTO types (name, img, color, api_endpoint) VALUES ('movies & shows', 'movie_filter', '#c70d3a', 'something');
 
-INSERT INTO types (name, img, color, api_endpoint) VALUES ('restaurants & cafes', 'restaurant', '#FF1744', 'another thing');
+INSERT INTO types (name, img, color, api_endpoint) VALUES ('restaurants & cafes', 'restaurant', '#ed5107', 'another thing');
 
-INSERT INTO types (name, img, color, api_endpoint) VALUES ('shopping', 'shopping_basket', '#B2FF59', 'quelque chose de pareil');
+INSERT INTO types (name, img, color, api_endpoint) VALUES ('shopping', 'shopping_basket', '#230338', 'quelque chose de pareil');
 
-INSERT INTO types (name, img, color, api_endpoint) VALUES ('books', 'menu_book', '#304FFE', 'different');
+INSERT INTO types (name, img, color, api_endpoint) VALUES ('books', 'menu_book', '#02383c', 'different');
