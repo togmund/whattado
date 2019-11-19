1cf4f2dbdf1fe23291360dcf1692366b1ae1a835
Ted Ogmundson <togmund@gmail.com>
2019-11-19 04:15:23 +0000

    changed all type badges to buttons from <a> tags so that the object that is passed has all the values

--- public/scripts/user-todo.js
+++ public/scripts/user-todo.js
@@ -1,27 +1,35 @@
 $(document).ready(() => {
 
   $("form.whattado.container").on("submit", function(event) {
+
     event.preventDefault();
+
     $(".todos.container").empty();
-    const $1h = $(this).children().children(".1h").val()
-    const $3h = $(this).children().children(".3h").val()
-    const $6h = $(this).children().children(".6h").val()
-    const $24h = $(this).children().children(".24h").val()
 
-    const $solo = $(this).children().children(".solo").val()
-    const $duo = $(this).children().children(".duo").val()
-    const $group = $(this).children().children(".group").val()
-    const $family = $(this).children().children(".family").val()
+    const $1h = $(this).find(".1h").val();
+    const $3h = $(this).find(".3h").val();
+    const $6h = $(this).find(".6h").val();
+    const $24h = $(this).find(".24h").val();
 
-    const $formValues = { $1h, $3h, $6h, $24h, $solo, $duo, $group, $family };
+    const $solo = $(this).find(".solo").val();
+    const $duo = $(this).find(".duo").val();
+    const $group = $(this).find(".group").val();
+    const $family = $(this).find(".family").val();
+
+    const $movies = $(this).find(".movies").val();
+    const $books = $(this).find(".books").val();
+    const $shopping = $(this).find(".shopping").val();
+    const $restaurants = $(this).find(".restaurants").val();
+
+    const $formValues = { $1h, $3h, $6h, $24h, $solo, $duo, $group, $family, $movies, $books, $shopping, $restaurants };
 
     $.ajax("/userTodos",{
       method: "GET",
       data: $formValues
     }).done((userTodos) => {
-      const toBeDone = userTodos.filter( u => !u.done );
+      const toBeDone = userTodos.filter(u => !u.done);
 
-      for(const userTodo of toBeDone) {
+      for (const userTodo of toBeDone) {
         const $todoContainer = $(`.todos.container`);
 
         const $article = $('<article>').addClass(`card horizontal`);
