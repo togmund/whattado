eff3ac6a2cad2339c2d242441cf0ede2807a15ae
Praneeth <praneethabc123@gmail.com>
2019-11-20 00:33:02 +0000

    adding axios to index and updated alltodos with promise all

--- routes/allTodos.js
+++ routes/allTodos.js
@@ -10,23 +10,40 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let queryString =`
-    SELECT todos.name as todo_name,types.name as type_name,todos.todo_id
-    FROM todos
-    JOIN types ON todos.type_id = types.type_id
-    WHERE todos.name ILIKE $1
-    ;`;
-    let values = ['%' + req.query.$search + '%']
-    db.query(queryString,values)
-      .then(data => {
-        const allTodos = data.rows;
-        res.json(allTodos);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    let searchText = req.query.$search;
+    let movieEndPoint = axios.get('http://www.omdbapi.com/?apikey=8dae3cd2&s='+searchText);
+    let restaurantEndPoint = axios.get('https://developers.zomato.com/api/v2.1/search?q='+searchText,{
+      headers: {
+        'user-key': '15be3dc7caf28a0303ceb8251bf19cec'
+    }});
+    Promise.all([movieEndPoint, restaurantEndPoint])
+    .then(values => Promise.all(values.map(value => value.json())))
+    .then(finalVals => {
+      let movieRes = finalVals[0];
+      let restaurantRes = finalVals[1];
+      console.log(movieRes);
+      console.log(restaurantRes);      // .then((res) => {
+        // .then((res) => {
+      //   const movies = res.data.search;
+      // })
+    // let queryString =`
+    // SELECT todos.name as todo_name,types.name as type_name,todos.todo_id
+    // FROM todos
+    // JOIN types ON todos.type_id = types.type_id
+    // WHERE todos.name ILIKE $1
+    // ;`;
+    // let values = ['%' + req.query.$search + '%']
+    // db.query(queryString,values)
+    //   .then(data => {
+    //     const allTodos = data.rows;
+    //     res.json(allTodos);
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    //   });
+    });
   });
   return router;
 };
