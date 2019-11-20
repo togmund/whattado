72aeb31c4042fc82e638df4df20e6d68fe1d4969
hildakh <h_khajehamiri@yahoo.com>
2019-11-20 03:39:33 +0000

    added the api call for google books.

--- routes/allTodos.js
+++ routes/allTodos.js
@@ -16,29 +16,33 @@ module.exports = ({db, axios}) => {
       headers: {
         'user-key': '15be3dc7caf28a0303ceb8251bf19cec'
     }});
-    Promise.all([movieEndPoint, restaurantEndPoint])
+    let bookEndPoint = axios.get('https://www.googleapis.com/books/v1/volumes?q=' + searchText);
+
+    Promise.all([movieEndPoint, restaurantEndPoint, bookEndPoint])
     .then(finalVals => {
       let movieRes = finalVals[0];
       let restaurantRes = finalVals[1];
+      let booksRes = finalVals[2];
       // console.log(movieRes.data);
-      console.log(restaurantRes.data.restaurants[0].restaurant.name);
-    let queryString =`
-    SELECT todos.name as todo_name,types.name as type_name,todos.todo_id
-    FROM todos
-    JOIN types ON todos.type_id = types.type_id
-    WHERE todos.name ILIKE $1
-    ;`;
-    let values = ['%' + req.query.$search + '%']
-    db.query(queryString,values)
-      .then(data => {
-        const allTodos = data.rows;
-        res.json(allTodos);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+      // console.log(restaurantRes.data.restaurants[0].restaurant.name);
+      console.log(booksRes.data.items[3].volumeInfo.title);
+    // let queryString =`
+    // SELECT todos.name as todo_name,types.name as type_name,todos.todo_id
+    // FROM todos
+    // JOIN types ON todos.type_id = types.type_id
+    // WHERE todos.name ILIKE $1
+    // ;`;
+    // let values = ['%' + req.query.$search + '%']
+    // db.query(queryString,values)
+    //   .then(data => {
+    //     const allTodos = data.rows;
+    //     res.json(allTodos);
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    //   });
     });
   });
   return router;
