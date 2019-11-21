df7ed84b4ea7068ade108936f99a464501207b96
hildakh <h_khajehamiri@yahoo.com>
2019-11-21 18:57:00 +0000

    updated the bookapi loop feed and appended the data from search results to containers

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -43,6 +43,59 @@ $(document).ready(function () {
         });
           const $divRightBottom = $('<div>').addClass('right-bottom');
 
+        $article.append($divLeft);
+          $divLeft.append($divLeftTop);
+            $divLeftTop.append($divLeftImg);
+          $divLeft.append($divLeftBottom);
+            $divLeftBottom.append($divLeftBottomBtn);
+        $article.append($divRight);
+          $divRight.append($divRightBottom);
+          $divRight.append($divRightTop);
+            $divRightTop.append($divRightTopText);
+            $divRightTop.append($divBtn);
+              $divBtn.append($doMeBtn);
+            $divRightTop.append($divRightTopText);
+              $divRightTopText.append($todoName);
+              $divRightTopText.append($author);
+              $divRightTopText.append($year);
+              $divRightTopText.append($genre);
+       $todoContainer.append($article);
+      }
+
+      for (todo of allTodos[1]) {
+
+        const $todoContainer = $(`.todos.container`);
+
+        const $article = $('<article>').addClass(`card horizontal`);
+        const $divLeft = $(`<div>`).addClass(`left`);
+        const $divLeftTop = $(`<div>`).addClass(`left-top`);
+        const $divLeftImg = $('<img>').addClass('image').attr('src',todo.volumeInfo.imageLinks.thumbnail);
+        const $divLeftBottom = $(`<div>`).addClass(`left-bottom`);
+        const $divLeftBottomBtn = $(`<a>`).addClass(`url`).attr({href: todo.volumeInfo.infoLink, target: '_blank'}).text('click');
+
+
+        const $divRight = $(`<div>`).addClass(`right`);
+        const $divRightTop = $(`<div>`).addClass(`right-top`);
+        const $divRightTopText = $(`<div>`).addClass(`right-top-text`);
+        const $todoName = $(`<div>`).addClass(`todo-name`).text(todo.volumeInfo.title);
+        const $author = $('<span>').addClass('author').text(todo.volumeInfo.authors);
+        const $year = $('<span>').addClass('year').text(todo.volumeInfo.publishedDate);
+        const $genre = $('<span>').addClass('genre').text(todo.volumeInfo.categories);
+        const $divBtn = $('<div>').addClass('right-top-btn');
+        const todoId = todo.todo_id;
+        // const apiId = todo.volumeInfo.industryIdentifiers[0].identifier;
+        const $doMeBtn = $('<button>').addClass('do-me').click(() => {
+          $.ajax(`/userTodos/${userTodosId}`, {
+            method: "PUT",
+            data: {todoId: todoId}
+            //for all todos
+            //data: {apiId : apiId }
+          }).done(() => {
+            // console.log('successfully marked as done', userTodo);
+          });
+        });
+          const $divRightBottom = $('<div>').addClass('right-bottom').text(todo.volumeInfo.averageRating);
+
 
         $article.append($divLeft);
           $divLeft.append($divLeftTop);
