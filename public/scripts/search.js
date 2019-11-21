9d8a0c5b02d5b4edbfad1bfa6456fff314ec0286
Ted Ogmundson <togmund@gmail.com>
2019-11-21 22:32:42 +0000

    merged all loops, all cards work now

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -19,7 +19,10 @@ $(document).ready(function () {
       method: "GET",
       data: { $search: $search }
     }).done((allTodos) => {
+
       $("#todo-spinner").hide();
+
+      // DB Result
       for (todo of allTodos[0]) {
         const $todoContainer = $(`.todos.container`);
 
@@ -70,8 +73,128 @@ $(document).ready(function () {
         $todoContainer.append($article);
       }
 
+      // Movie API result
+
+      for (todo of allTodos[1]) {
+        const $todoContainer = $(`.todos.container`);
+
+        const $article = $('<article>').addClass(`card horizontal movies`);
+        const $divLeft = $(`<div>`).addClass(`left`);
+        const $divLeftTop = $(`<div>`).addClass(`left-top`);
+        const $divLeftImg = $('<img>').addClass('image').attr('src', todo.Poster);
+        const $divLeftBottom = $(`<div>`).addClass(`left-bottom`);
+        const $divLeftBottomBtn = $(`<a>`).addClass(`url`).attr({ href: `https://imdb.com/title/` + todo.imdbID, target: '_blank' }).text('IMDB');
+
+
+        const $divRight = $(`<div>`).addClass(`right`);
+        const $divRightTop = $(`<div>`).addClass(`right-top`);
+        const $divRightTopText = $(`<div>`).addClass(`right-top-text`);
+        const $todoName = $(`<div>`).addClass(`todo-name`).text(todo.Title);
+        const $author = $('<span>').addClass('author').text(todo.Type);
+        const $year = $('<span>').addClass('year').text(todo.Year);
+        const $genre = $('<span>').addClass('genre');
+        const $divBtn = $('<div>').addClass('right-top-btn');
+        const $doMeBtn = $('<button>').addClass('do-me').click(() => {
+          $.ajax(`/allTodos/new`, {
+            method: "PUT",
+            data: {
+              user_id: req.session.userId,
+              api_id: todo.imdbID,
+              name: todo.Title,
+              subtype: todo.Type,
+              year: todo.Year,
+              url: `https://imdb.com/title/` + todoId,
+              img: todo.Poster,
+              type_id: 1
+            }
+          }).done(() => {
+            // console.log('successfully marked as done', userTodo);
+          });
+        });
+        const $divRightBottom = $('<div>').addClass('right-bottom');
+
+
+        $article.append($divLeft);
+        $divLeft.append($divLeftTop);
+        $divLeftTop.append($divLeftImg);
+        $divLeft.append($divLeftBottom);
+        $divLeftBottom.append($divLeftBottomBtn);
+        $article.append($divRight);
+        $divRight.append($divRightBottom);
+        $divRight.append($divRightTop);
+        $divRightTop.append($divRightTopText);
+        $divRightTop.append($divBtn);
+        $divBtn.append($doMeBtn);
+        $divRightTop.append($divRightTopText);
+        $divRightTopText.append($todoName);
+        $divRightTopText.append($author);
+        $divRightTopText.append($year);
+        $divRightTopText.append($genre);
+        $todoContainer.append($article);
+      }
+
+      // Books api
+      for (todo of allTodos[2]) {
+        const $todoContainer = $(`.todos.container`);
+        const $article = $('<article>').addClass(`card horizontal books`);
+        const $divLeft = $(`<div>`).addClass(`left`);
+        const $divLeftTop = $(`<div>`).addClass(`left-top`);
+        let $divLeftImg = $('<img>').addClass('image circle');
+        if (todo.volumeInfo.imageLinks) {
+          $divLeftImg = $('<img>').addClass('image circle').attr('src', todo.volumeInfo.imageLinks.thumbnail);
+        } else {
+          $divLeftImg = $('<img>').addClass('image circle').text("NO IMAGE");
+        }
+        const $divLeftBottom = $(`<div>`).addClass(`left-bottom`);
+        const $divLeftBottomBtn = $(`<a>`).addClass(`url`).attr({ href: todo.volumeInfo.infoLink, target: '_blank' }).text('click');
+        const $divRight = $(`<div>`).addClass(`right`);
+        const $divRightTop = $(`<div>`).addClass(`right-top`);
+        const $divRightTopText = $(`<div>`).addClass(`right-top-text`);
+        const $todoName = $(`<div>`).addClass(`todo-name`).text(todo.volumeInfo.title);
+        const $author = $('<span>').addClass('author').text(todo.volumeInfo.authors);
+        const $year = $('<span>').addClass('year').text(todo.volumeInfo.publishedDate);
+        const $genre = $('<span>').addClass('genre').text(todo.volumeInfo.categories);
+        const $divBtn = $('<div>').addClass('right-top-btn');
+        // const todoId = todo.todo_id;
+        const $doMeBtn = $('<button>').addClass('do-me').click(() => {
+          $.ajax(`/allTodos/new`, {
+            method: "PUT",
+            data: {
+              user_id: req.session.userId,
+              type_id: 2,
+              api_id: todo.volumeInfo.industryIdentifiers[0].identifier,
+              author: todo.volumeInfo.authors,
+              url: todo.volumeInfo.infoLink,
+              name: todo.volumeInfo.title,
+              user_rating: todo.volumeInfo.averageRating,
+              img: todo.volumeInfo.imageLinks.thumbnail
+            }
+          }).done(() => {
+            // console.log('successfully marked as done', userTodo);
+          });
+        });
+        const $divRightBottom = $('<div>').addClass('right-bottom').text(todo.volumeInfo.averageRating);
+        $article.append($divLeft);
+        $divLeft.append($divLeftTop);
+        $divLeftTop.append($divLeftImg);
+        $divLeft.append($divLeftBottom);
+        $divLeftBottom.append($divLeftBottomBtn);
+        $article.append($divRight);
+        $divRight.append($divRightBottom);
+        $divRight.append($divRightTop);
+        $divRightTop.append($divRightTopText);
+        $divRightTop.append($divBtn);
+        $divBtn.append($doMeBtn);
+        $divRightTop.append($divRightTopText);
+        $divRightTopText.append($todoName);
+        $divRightTopText.append($author);
+        $divRightTopText.append($year);
+        $divRightTopText.append($genre);
+        $todoContainer.append($article);
+      }
+
       // Album Card Builder
-      for (todo of allTodos[0]) {
+      for (todo of allTodos[3]) {
 
         const $todoContainer = $(`.todos.container`);
 
@@ -131,18 +254,18 @@ $(document).ready(function () {
         $todoContainer.append($article);
       }
 
-
       // Artist Card Builder
-      for (todo of allTodos[1]) {
+      for (todo of allTodos[4]) {
         const $todoContainer = $(`.todos.container`);
 
         const $article = $('<article>').addClass(`card horizontal music`);
         const $divLeft = $(`<div>`).addClass(`left`);
         const $divLeftTop = $(`<div>`).addClass(`left-top`);
+        let $divLeftImg = $('<img>').addClass('image circle');
         if (todo.images.length !== 0) {
-          const $divLeftImg = $('<img>').addClass('image circle').attr('src', todo.images[0].url);
+          $divLeftImg = $('<img>').addClass('image circle').attr('src', todo.images[0].url);
         } else {
-          const $divLeftImg = $('<img>').addClass('image circle').text("NO IMAGE");
+          $divLeftImg = $('<img>').addClass('image circle').text("NO IMAGE");
         }
         const $divLeftBottom = $(`<div>`).addClass(`left-bottom`);
         const $divLeftBottomBtn = $(`<a>`).addClass(`url`).text(todo.external_urls.spotify);
@@ -194,62 +317,9 @@ $(document).ready(function () {
         $divRightTopText.append($genre);
         $todoContainer.append($article);
       }
-      // Books api
-      for (todo of allTodos[1]) {
-        const $todoContainer = $(`.todos.container`);
-        const $article = $('<article>').addClass(`card horizontal books`);
-        const $divLeft = $(`<div>`).addClass(`left`);
-        const $divLeftTop = $(`<div>`).addClass(`left-top`);
-        const $divLeftImg = $('<img>').addClass('image, circle').attr('src', todo.volumeInfo.imageLinks.thumbnail);
-        const $divLeftBottom = $(`<div>`).addClass(`left-bottom`);
-        const $divLeftBottomBtn = $(`<a>`).addClass(`url`).attr({ href: todo.volumeInfo.infoLink, target: '_blank' }).text('click');
-        const $divRight = $(`<div>`).addClass(`right`);
-        const $divRightTop = $(`<div>`).addClass(`right-top`);
-        const $divRightTopText = $(`<div>`).addClass(`right-top-text`);
-        const $todoName = $(`<div>`).addClass(`todo-name`).text(todo.volumeInfo.title);
-        const $author = $('<span>').addClass('author').text(todo.volumeInfo.authors);
-        const $year = $('<span>').addClass('year').text(todo.volumeInfo.publishedDate);
-        const $genre = $('<span>').addClass('genre').text(todo.volumeInfo.categories);
-        const $divBtn = $('<div>').addClass('right-top-btn');
-        // const todoId = todo.todo_id;
-        const $doMeBtn = $('<button>').addClass('do-me').click(() => {
-          $.ajax(`/allTodos/new`, {
-            method: "PUT",
-            data: {
-              user_id: req.session.userId,
-              type_id: 2,
-              api_id: todo.volumeInfo.industryIdentifiers[0].identifier,
-              author: todo.volumeInfo.authors,
-              url: todo.volumeInfo.infoLink,
-              name: todo.volumeInfo.title,
-              user_rating: todo.volumeInfo.averageRating,
-              img: todo.volumeInfo.imageLinks.thumbnail
-            }
-          }).done(() => {
-            // console.log('successfully marked as done', userTodo);
-          });
-        });
-        const $divRightBottom = $('<div>').addClass('right-bottom').text(todo.volumeInfo.averageRating);
-        $article.append($divLeft);
-        $divLeft.append($divLeftTop);
-        $divLeftTop.append($divLeftImg);
-        $divLeft.append($divLeftBottom);
-        $divLeftBottom.append($divLeftBottomBtn);
-        $article.append($divRight);
-        $divRight.append($divRightBottom);
-        $divRight.append($divRightTop);
-        $divRightTop.append($divRightTopText);
-        $divRightTop.append($divBtn);
-        $divBtn.append($doMeBtn);
-        $divRightTop.append($divRightTopText);
-        $divRightTopText.append($todoName);
-        $divRightTopText.append($subType);
-        $divRightTopText.append($genre);
-        $todoContainer.append($article);
-      }
 
       // Track Card Builder
-      for (todo of allTodos[2]) {
+      for (todo of allTodos[5]) {
         console.log(todo);
         const $todoContainer = $(`.todos.container`);
 
@@ -303,6 +373,67 @@ $(document).ready(function () {
         $divRightTop.append($divRightTopText);
         $divRightTopText.append($todoName);
         $divRightTopText.append($subType);
+        $todoContainer.append($article);
+      }
+
+      // Restaurant API results
+      for (todo of allTodos[6]) {
+
+        const $todoContainer = $(`.todos.container`);
+
+        const $article = $('<article>').addClass(`card horizontal restaurants`);
+        const $divLeft = $(`<div>`).addClass(`left`);
+        const $divLeftTop = $(`<div>`).addClass(`left-top`);
+        const $divLeftImg = $('<img>').addClass('image').attr('src', todo.restaurant.thumb);
+        const $divLeftBottom = $(`<div>`).addClass(`left-bottom`);
+        const $divLeftBottomBtn = $(`<a>`).addClass(`url`).attr({ href: todo.restaurant.url, target: '_blank' }).text('Zomato');
+
+
+        const $divRight = $(`<div>`).addClass(`right`);
+        const $divRightTop = $(`<div>`).addClass(`right-top`);
+        const $divRightTopText = $(`<div>`).addClass(`right-top-text`);
+        const $todoName = $(`<div>`).addClass(`todo-name`).text(todo.restaurant.name);
+        const $author = $('<span>').addClass('author').text(todo.restaurant.location.address);
+        const $year = $('<span>').addClass('year');
+        const $genre = $('<span>').addClass('genre').text(todo.restaurant.cuisines);
+        const $divBtn = $('<div>').addClass('right-top-btn');
+        const $doMeBtn = $('<button>').addClass('do-me').click(() => {
+          $.ajax(`/allTodos/new`, {
+            method: "PUT",
+            data: {
+              user_id: req.session.userId,
+              api_id: todo.restaurant.id,
+              name: todo.restaurant.name,
+              location: todo.restaurant.location.address,
+              genre: todo.restaurant.cuisines,
+              url: todo.restaurant.url,
+              img: todo.restaurant.thumb,
+              user_rating: todo.restaurant.user_ratings.aggregate_rating,
+              type_id: 4
+            }
+          }).done(() => {
+            // console.log('successfully marked as done', userTodo);
+          });
+        });
+        const $divRightBottom = $('<div>').addClass('right-bottom');
+
+
+        $article.append($divLeft);
+        $divLeft.append($divLeftTop);
+        $divLeftTop.append($divLeftImg);
+        $divLeft.append($divLeftBottom);
+        $divLeftBottom.append($divLeftBottomBtn);
+        $article.append($divRight);
+        $divRight.append($divRightBottom);
+        $divRight.append($divRightTop);
+        $divRightTop.append($divRightTopText);
+        $divRightTop.append($divBtn);
+        $divBtn.append($doMeBtn);
+        $divRightTop.append($divRightTopText);
+        $divRightTopText.append($todoName);
+        $divRightTopText.append($author);
+        $divRightTopText.append($year);
+        $divRightTopText.append($genre);
         $todoContainer.append($article);
       }
 
--- routes/allTodos.js
+++ routes/allTodos.js
@@ -18,7 +18,6 @@ module.exports = ({ db, axios }) => {
     // Sanitize
     const searchText = req.query.$search;
 
-
     // DB Query
     const queryString = `
     SELECT *,todos.img as todo_img, todos.name as todo_name,types.name as type_name,todos.todo_id
@@ -27,53 +26,42 @@ module.exports = ({ db, axios }) => {
     WHERE todos.name ILIKE $1
     ;`;
     const values = ['%' + searchText + '%']
-    // const todoEndpoint = db.query(queryString, values)
+    const todoEndpoint = db.query(queryString, values)
 
     // API Endpoint Function Delclarations
-    // const movieEndPoint = axios.get('http://www.omdbapi.com/?apikey=8dae3cd2&s=' + searchText);
-    // const bookEndPoint = axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchText}&key=AIzaSyBi1b3U6fVBvIo4VwCylsVDkDY-Aph6BX8`);
+    const movieEndPoint = axios.get('http://www.omdbapi.com/?apikey=8dae3cd2&s=' + searchText);
+    const bookEndPoint = axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchText}&key=AIzaSyBi1b3U6fVBvIo4VwCylsVDkDY-Aph6BX8`);
     const musicEndPoint = axios.get(`https://api.spotify.com/v1/search?q=${searchText}&type=track%2Cartist%2Calbum`, {
       headers: {
         'Authorization': 'Bearer ' + musicAccessToken
       }
     });
-    // const restaurantEndPoint = axios.get('https://developers.zomato.com/api/v2.1/search?q=' + searchText, {
-    //   headers: {
-    //     'user-key': '15be3dc7caf28a0303ceb8251bf19cec'
-    //   }
-    // });
-
+    const restaurantEndPoint = axios.get('https://developers.zomato.com/api/v2.1/search?q=' + searchText, {
+      headers: {
+        'user-key': '15be3dc7caf28a0303ceb8251bf19cec'
+      }
+    });
 
     // Promise to return API results
-    // Promise.all([todoEndpoint, movieEndPoint, /* bookEndPoint, */ /*musicEndPoint,*/ restaurantEndPoint])
-
-    Promise.all([musicEndPoint])
+    Promise.all([todoEndpoint, movieEndPoint, bookEndPoint, musicEndPoint, restaurantEndPoint])
       .then(finalVals => {
 
-        // const todoRes = finalVals[0].rows;
-        // const movieRes = finalVals[1];
-        // // const booksRes = finalVals[2];
-        const albumRes = finalVals[0].data.albums.items;
-        const artistRes = finalVals[0].data.artists.items;
-        const trackRes = finalVals[0].data.tracks.items;
-        // const restaurantRes = finalVals[3];
-        // console.log([
-        //   todoRes,
-        //   movieRes,
-        //   /* booksRes, */
-        //   albumRes,
-        //   artistRes,
-        //   trackRes,
-        //   restaurantRes
-        // ])
+        const todoRes = finalVals[0].rows;
+        const movieRes = finalVals[1].data.Search;
+        const booksRes = finalVals[2].data.items;
+        const albumRes = finalVals[3].data.albums.items;
+        const artistRes = finalVals[3].data.artists.items;
+        const trackRes = finalVals[3].data.tracks.items;
+        const restaurantRes = finalVals[4].data.restaurants;
+
         res.json([
-          // todoRes
-          // movieRes,
-          /* booksRes, */
+          todoRes,
+          movieRes,
+          booksRes,
           albumRes,
           artistRes,
-          trackRes
-          // restaurantRes
+          trackRes,
+          restaurantRes
         ])
       })
       .catch(err => {
